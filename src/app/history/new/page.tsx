bfab1fc97c75f45b725f87e58dbb3bce09828ae3
Imri Naim <imri.n@gloat.com>
2025-11-28 10:06:01 +0200

    fix ts

--- src/app/history/new/page.tsx
+++ src/app/history/new/page.tsx
@@ -1,10 +1,14 @@
 import { NewWorkoutClient } from "@/app/history/new/new-workout-client";
 import { extractSearchParam } from "@/lib/utils";
 
-export default async function NewWorkoutPage(props: PageProps<"/history/new">) {
-  const searchParams = await props.searchParams;
-  const date = extractSearchParam(searchParams.date);
-  const templateId = extractSearchParam(searchParams.templateId);
+export default async function NewWorkoutPage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const resolvedSearchParams = await searchParams;
+  const date = extractSearchParam(resolvedSearchParams.date);
+  const templateId = extractSearchParam(resolvedSearchParams.templateId);
 
   return <NewWorkoutClient date={date} templateId={templateId} />;
 }
